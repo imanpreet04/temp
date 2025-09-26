f0f6f5fb9ec5071efe32eb57b4df6c37c7ce03c4
gavy04 <imanpreet@pantomax.co>
2025-09-26 17:16:53 +0530

    f

--- index.js
+++ index.js
@@ -3,29 +3,33 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello World
+  res.send('Hello World! ');
 });
 
-app.get('/about', (req, res) => {
-  re is the about page');
+app.get('/about ', (req, res) => {
+  res.send('This is the about page');
 });
 
 app.get('/user/:id', (req, res) => {
   const userId = req.params.id;
-  res.send(`User ID is: ${userId}`);
+  res.send(`User ID is: ${userid}`);
 });
 
 app.get('/contact', (req, res) => {
   if (req.query.name) {
-    reme');
+    res.send(`Hello ${req.query.name}`);
   }
 });
 
 app.get('/duplicate', (req, res) => {
-  res.send('This is the duplicate route (only defined once now)');
+  res.send('First duplicate endpoint');
+});
+
+app.get('/duplicate', (req, res) => {
+  res.send('Second duplicate endpoint (this one always wins)');
 });
 
 const PORT = 3000;
 app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
+  console.log(`Server running at http://localhost:3000`);
 });
